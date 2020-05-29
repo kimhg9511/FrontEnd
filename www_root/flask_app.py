21ec27adf0d7d9a983e3515841b11b2789fb6191
kimhg <kimhgedu@users.noreply.github.com>
2020-05-29 14:45:57 +0900

    [2020-05-29] directory update for flask(templates)

--- www_root/flask_app.py
+++ www_root/flask_app.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, request, session, render_template, redirect, url_for
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
     str = 'hello'
     return str
-    
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard/html/index.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('mogo/html/Mogo.html')
+
+
+@app.route('/storyboard')
+def dashboard():
+    return render_template('storyboard/html/index0.html')
+
+
+if __name__ == '__main__':
+    hello()
+    app.debug = True
+    app.run()
